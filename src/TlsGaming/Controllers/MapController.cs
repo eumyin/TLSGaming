1fb7d02ffafdec86ab4a52e77ca536e5521aa8da
Brian Engel <brianengel@gmail.com>
2015-12-05 16:17:51 -0500

    Added map/rules section

---
+++ src/TlsGaming/Controllers/MapController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TlsGaming.Controllers
+{
+    [Route("map")]
+    public class MapController : Controller
+    {
+        public MapController()
+        {
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TlsGaming/Controllers/RulesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TlsGaming.Controllers
+{
+    [Route("rules")]
+    public class RulesController : Controller
+    {
+        public RulesController()
+        {
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
